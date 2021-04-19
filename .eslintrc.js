fce44c7a39b408b742ab33e6da6053a16fb173cb
huangli <1833002417@qq.com>
2021-04-19 22:56:55 +0800

    feat:组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
-module.exports = {
+module.exports = { // eslint-disable-line
   root: true,
   env: {
     node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
+    '@vue/standard', // https://standardjs.com/rules-zhcn.html
     '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
+    quotes: [1, "double"],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <div class = "container"></div>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+export default defineComponent({
+  name: "App"
+})
+</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass" scoped>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
